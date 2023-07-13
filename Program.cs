9d7e3db3be54c816b275a1ee8ca8de9cc2738f35
Al Mais <mais2820@gmail.com>
2023-07-13 07:24:32 +0700

    Baru menu region yang berhasil

--- Program.cs
+++ Program.cs
@@ -40,5 +40,215 @@ public class Program
 
         //GetCountries();
         //InsertCountries("MY", "Malaysia", 13);
+
+        bool exit = false;
+
+        while (!exit)
+        {
+            Console.WriteLine("== MENU ==");
+            Console.WriteLine("1. Employees");
+            Console.WriteLine("2. Departments");
+            Console.WriteLine("3. Jobs");
+            Console.WriteLine("4. Histories");
+            Console.WriteLine("5. Locations");
+            Console.WriteLine("6. Countries");
+            Console.WriteLine("7. Regions");
+            Console.WriteLine("8. Exit");
+
+            Console.Write("Input Menu: ");
+            int menuChoice = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+            switch (menuChoice)
+            {
+                case 1:
+                    ProcessSubMenuEmployees();
+                    break;
+                case 2:
+                    ProcessSubMenuDepartment();
+                    break;
+                case 3:
+                    ProcessSubMenuJobs();
+                    break;
+                case 4:
+                    ProcessSubMenuHistories();
+                    break;
+                case 5:
+                    ProcessSubMenuLocation();
+                    break;
+                case 6:
+                    ProcessSubMenuCountries();
+                    break;
+                case 7:
+                    ProcessSubMenuRegion();
+                    Console.ReadLine();
+                    break;
+                case 8:
+                    exit = true;
+                    break;
+                default:
+                    Console.WriteLine("Invalid, coba lagi.");
+                    break;  
+            }
+            Console.WriteLine();
+            Console.Clear();
+        }
+
+        static void ProcessSubMenuRegion()
+        {
+            bool back = false;
+
+            while (!back)
+            {
+                Console.WriteLine("\n== REGION ==");
+
+                Console.WriteLine("1. Get");
+                Console.WriteLine("2. GetById");
+                Console.WriteLine("3. Insert");
+                Console.WriteLine("4. Update");
+                Console.WriteLine("5. Delete");
+                Console.WriteLine("6. Back");
+
+                Console.Write("Input Menu: ");
+                int subMenuChoice = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+
+                switch (subMenuChoice)
+                {
+                    case 1:
+                        Region.GetRegions();
+                        Console.ReadLine();
+                        break;
+                    case 2:
+                        Console.Write("Input Region ID: ");
+                        int regionGetId = Convert.ToInt32(Console.ReadLine());
+                        Region.GetRegionsById(regionGetId);
+                        Console.ReadLine();
+                        break;
+                    case 3:
+                        Console.Write("Input Region ID: ");
+                        int regionId = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Input Region Name: ");
+                        string regionName = Console.ReadLine();
+                        Region.InsertRegions(regionId, regionName);
+                        Console.ReadLine();
+                        break;
+                    case 4:
+                        Console.Write("Input Region ID: ");
+                        int updateRegionId = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Input Region Name: ");
+                        string updateRegionName = Console.ReadLine();
+                        Region.UpdateRegions(updateRegionId, updateRegionName);
+                        Console.ReadLine();
+                        break;
+                    case 5:
+                        Console.Write("Input Region ID: ");
+                        int deleteRegionId = Convert.ToInt32(Console.ReadLine());
+                        Region.DeleteRegions(deleteRegionId);
+                        Console.ReadLine();
+                        break;
+                    case 6:
+                        back = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid menu choice. Please try again.");
+                        break;
+                }
+                Console.Clear();
+            }
+        }
+
+        static void ProcessSubMenuEmployees()
+        {
+            Console.WriteLine("Invalid, coba lagi.");
+        }
+
+        static void ProcessSubMenuDepartment()
+        {
+            Console.WriteLine("Invalid, coba lagi.");
+        }
+
+        static void ProcessSubMenuJobs()
+        {
+            bool back = false;
+
+            while (!back)
+            {
+                Console.WriteLine("\n== JOBS ==");
+
+                Console.WriteLine("1. Get");
+                Console.WriteLine("2. GetById");
+                Console.WriteLine("3. Insert");
+                Console.WriteLine("4. Update");
+                Console.WriteLine("5. Delete");
+                Console.WriteLine("6. Back");
+
+                Console.Write("Input Menu: ");
+                int subMenuChoice = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+
+                switch (subMenuChoice)
+                {
+                    case 1:
+                        Region.GetRegions();
+                        Console.ReadLine();
+                        break;
+                    case 2:
+                        Console.Write("Input Region ID: ");
+                        int regionGetId = Convert.ToInt32(Console.ReadLine());
+                        Region.GetRegionsById(regionGetId);
+                        Console.ReadLine();
+                        break;
+                    case 3:
+                        Console.Write("Input Region ID: ");
+                        int regionId = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Input Region Name: ");
+                        string regionName = Console.ReadLine();
+                        Region.InsertRegions(regionId, regionName);
+                        Console.ReadLine();
+                        break;
+                    case 4:
+                        Console.Write("Input Region ID: ");
+                        int updateRegionId = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Input Region Name: ");
+                        string updateRegionName = Console.ReadLine();
+                        Region.UpdateRegions(updateRegionId, updateRegionName);
+                        Console.ReadLine();
+                        break;
+                    case 5:
+                        Console.Write("Input Region ID: ");
+                        int deleteRegionId = Convert.ToInt32(Console.ReadLine());
+                        Region.DeleteRegions(deleteRegionId);
+                        Console.ReadLine();
+                        break;
+                    case 6:
+                        back = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid menu choice. Please try again.");
+                        break;
+                }
+                Console.Clear();
+            }
+        }
+
+        static void ProcessSubMenuHistories()
+        {
+            Console.WriteLine("Invalid, coba lagi.");
+        }
+
+        static void ProcessSubMenuLocation()
+        {
+            Console.WriteLine("Invalid, coba lagi.");
+        }
+
+        static void ProcessSubMenuCountries()
+        {
+            Console.WriteLine("Invalid, coba lagi.");
+        }
+
+
+
+
     }
 }
